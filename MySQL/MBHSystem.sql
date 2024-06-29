61300f55fa8a953bcbad4f044fdb30f5d887851b
Bahir Hussain <mbahirh624@gmail.com>
2024-06-29 13:58:47 +0530

    update

--- MySQL/MBHSystem.sql
+++ MySQL/MBHSystem.sql
@@ -1,2 +1,9 @@
 create database MBHSystem;
 show databases;
+create table employee (EmpId int primary key,
+Name varchar(50),
+Department varchar(50));
+
+create table Department (DepartmentId int primary key,
+Name varchar(50),
+DepartmentType varchar(50));
