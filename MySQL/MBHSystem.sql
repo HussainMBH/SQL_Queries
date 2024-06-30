fc633a6ce51e3324803eaf86c018533b4919e428
Bahir Hussain <mbahirh624@gmail.com>
2024-06-30 11:43:25 +0530

    Update

--- MySQL/MBHSystem.sql
+++ MySQL/MBHSystem.sql
@@ -7,3 +7,27 @@ Department varchar(50));
 create table Department (DepartmentId int primary key,
 Name varchar(50),
 DepartmentType varchar(50));
+
+create table DepartmentCategory (DepartmentId int primary key,
+CategoryName varchar(50),
+CategoryType varchar(50));
+
+use mbhsystem;
+use banking_app;
+create table DepartmentCategory (DepartmentId int primary key,
+CategoryName varchar(50),
+CategoryType varchar(50));
+
+drop table department;
+use mbhsystem;
+drop table department;
+drop table departmentcategory, employee;
+
+create table StaffDetails(staffId int primary key,
+Name varchar(50),
+Department varchar(50),
+Contactno int(10),
+StaffPosition varchar(50)
+);
+
+
